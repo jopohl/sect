5cdc11682247dfba24a90df5a10e865d06f4b62c
jopohl <Johannes.Pohl90@gmail.com>
2020-06-22 20:25:50 +0200

    add tests

--- ElliptischeKurvenTests/MathTest.cs
+++ ElliptischeKurvenTests/MathTest.cs
@@ -11,7 +11,37 @@ namespace EllipticCurveTests
         public void TestCubicSolve()
         {
             double result1 = MathExtensions.SolveReducedCubicEquation(2, 2)[0];
-            Assert.AreEqual(Math.Round(result1,2),-0.77);
+            Assert.AreEqual(Math.Round(result1, 2), -0.77);
+        }
+
+        [TestMethod]
+        public void TestModulo()
+        {
+            Assert.AreEqual(5.Mod(3), 2);
+            Assert.AreEqual(42.Mod(42), 0);
+            Assert.AreEqual(5.Mod(-3), 2);
+        }
+
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            Assert.IsTrue(5.IsPrime());
+            Assert.IsTrue(7.IsPrime());
+            Assert.IsTrue(11.IsPrime());
+            Assert.IsTrue(2.IsPrime());
+            Assert.IsFalse(1.IsPrime());
+            Assert.IsFalse(1.IsPrime());
+            Assert.IsFalse(4.IsPrime());
+            Assert.IsFalse(42.IsPrime());
+            Assert.IsFalse(21.IsPrime());
+        }
+
+        [TestMethod]
+        public void TestMultInv()
+        {
+            Assert.AreEqual(5.MultInv(7), 3);
+            Assert.AreEqual(2.MultInv(8), 0);  // no inverse
+            Assert.AreEqual(2.MultInv(5), 3);
         }
     }
 }
