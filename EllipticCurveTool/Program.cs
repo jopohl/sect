c41731921220e6f1ee07e2fd05029003e7f794e9
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-06-24 17:05:39 +0200

    Add GitHub actions (#3)

--- EllipticCurveTool/Program.cs
+++ EllipticCurveTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 using System.Windows.Forms;
 using EllipticCurveTool.Controller;
 
@@ -6,13 +7,35 @@ namespace EllipticCurveTool
 {
     static class Program
     {
+        private static System.Timers.Timer _closeTimer;
+        
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             MainController controller = new MainController();
-            Application.Run(controller.Form);
+
+            if (args.Length == 1 && args[0] == "autoclose")
+            {
+                _closeTimer = new System.Timers.Timer(1000);
+                _closeTimer.Elapsed += OnTimedEvent;
+                _closeTimer.AutoReset = false;
+                _closeTimer.Start();
+                Console.WriteLine("Starting application...");
+                Application.Run(controller.Form);
+                _closeTimer.Stop();
+                _closeTimer.Dispose();
+            }
+            else
+            {
+                Application.Run(controller.Form);
+            }
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Application.Exit();
+            Console.WriteLine("Application was automatically closed.");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # sect
 Simple Elliptic Curve Tool
 
+This tool runs on Windows and Linux without installation. Download the latest version from the [releases page](https://github.com/jopohl/sect/releases) and follow the instructions for your OS below.
+
+##### Windows
+Simply run ```Simple_Elliptic_Curve_Tool.exe```. 
+
+##### Linux
+Install Mono & run ```mono Simple_Elliptic_Curve_Tool.exe```. 
+
+##### macOS (Unsupported)
+Unfortunately, this application does __not run on macOS 10.15 Catalina and above__ due to https://github.com/mono/mono/issues/17301.
+
+If you use a macOS version <10.15, you may be lucky by installing Mono & running ```mono --arch=32 Simple_Elliptic_Curve_Tool.exe```. 
+
 # Screenshots
 
